5bceedf93bc2391931dfdfca0f42f57a57f12b73
Lucasboreto_ <145051990+lucasBoreto@users.noreply.github.com>
2024-12-13 21:10:01 -0300

    Cadastro de Produtos 2
    - Atualização do codigo do modulo classe_produtos
    (Exclusão do método salvar_produto; refatoração da variavel preços, que foi substituida por preço_credito, preco_debito e preco_pix)
    - Adição de um match case na main
    (case 1 - cadastro de produto, case 2 - sair da aplicação)

--- Sistema_Vendas/modulos/classe_Produtos.py
+++ Sistema_Vendas/modulos/classe_Produtos.py
@@ -1,17 +1,18 @@
-class Produtos:
-    def  __init__(self, codigo, nome='ITEM', precos={'CRÉDITO':0, 'DÉBITO':0, 'DINHEIRO':0 }, quantidade=0, tipo='', marca=''):
+class Produto:
+    """Classe dos Produtos
+    - Cada produto possui seu codigo, nome, preços, quantidade, tipo e marca, caso tenha"""
+    def  __init__(self, codigo, nome='ITEM', preco_credito=0, preco_debito=0, preco_pix=0, quantidade=0, tipo='', marca=''):
         self.codigo = codigo
         self.nome = nome.upper()
         self.marca = marca.upper()
         self.tipo = tipo.upper()
-        self.precos = [precos[0], precos[1], precos[2]]
+        self.preco_credito, self.preco_debito, self.preco_pix = preco_credito, preco_debito, preco_pix
         self.quantidade = quantidade
 
     def listar_info(self):
+        """- Função para listar informações do produto de forma formatada"""
         print('-'*70)
         print(f"{'COD':^10}{'NOME':^10}{'MARCA':^10}{'TIPO':^10}{'PREÇOS':^10}{'QUANTIDADE':^10}")
-        print(f'{self.codigo:^10}{self.nome:^10}{self.marca:^10}{self.tipo:^10}{self.precos[0]:^3}{self.precos[1]:^3}{self.precos[2]:^3}{self.quantidade:^10}')
+        print(f'{self.codigo:^10}{self.nome:^10}{self.marca:^10}{self.tipo:^10}{self.preco_credito:^3}{self.preco_debito:^3}{self.preco_pix:^3}{self.quantidade:^10}')
         print('-'*70)
 
-    def salvar_produto(self):
-        produto = open()
